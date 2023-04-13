1fa74dbf0db769d2085fa0352d86b13644c7db72
LeonardoMateo1 <104122352+LeonardoMateo1@users.noreply.github.com>
2023-04-13 02:29:32 -0400

    added hover animation to the link class /small fix

--- client/src/pages/Nav.jsx
+++ client/src/pages/Nav.jsx
@@ -25,23 +25,23 @@ const Nav = () => {
             <ul className='flex flex-col items-center justify-around w-full gap-4 pt-6 pb-8 bg-black md:flex-row'>
                 <li className='sm:text-3xl lg:text-5xl'><img src={logo} alt="logo"/></li>
                 <li className='hidden font-normal lg:flex gap-7 white'>
-                    <p><Link to='/'>Home</Link></p>
-                    <p><Link to='/Menu'>Menu</Link> </p>
-                    <p><Link to='/Events'>Events</Link> </p>
-                    <p><Link to='/Blog'>Blog</Link> </p>
-                    <p><Link to='/Contact_Us'>Contact Us</Link></p>
+                    <p className='link'><Link to='/'>Home</Link></p>
+                    <p className='link'><Link to='/Menu'>Menu</Link> </p>
+                    <p className='link'><Link to='/Events'>Events</Link> </p>
+                    <p className='link'><Link to='/Blog'>Blog</Link> </p>
+                    <p className='link'><Link to='/Contact_Us'>Contact Us</Link></p>
                 </li>
                 <li className='flex gap-2 font-[600]'>
                     <div className='hidden lg:block'>
                         <div className='flex gap-2'>
                             <div className='relative items-center inline-block'>
-                                <div className='inline-flex items-center gap-1 hover:text-red-500'>
-                                    <button onClick={toggleGiftDropdown}>Gift Cards</button>
-                                    <IoMdArrowDropdown className='text-red-500'/>
+                                <div className='hover:text-red-500'>
+                                    <button className='flex items-center gap-1' onClick={toggleGiftDropdown}>Gift Cards <IoMdArrowDropdown className='text-red-500'/></button>
+                                    
                                 </div>
                                 <ul className={`absolute ${isGiftOpen ? '' : 'hidden'} bg-black p-3 gap-2 flex flex-col w-40 left-[-11px]`}>
-                                    <li><a href='https://www.toasttab.com/lamoraga/giftcards'>Buy Gift Card</a></li>
-                                    <li><a href='https://www.toasttab.com/lamoraga/findcard'>Check Balance</a></li>
+                                    <li><a href='https://www.toasttab.com/lamoraga/giftcards' className='link'>Buy Gift Card</a></li>
+                                    <li><a href='https://www.toasttab.com/lamoraga/findcard' className='link'>Check Balance</a></li>
                                 </ul>
                             </div>
                             <span>|</span>
@@ -57,12 +57,12 @@ const Nav = () => {
                                 <button onClick={toggleMenuDropdown}><MdRestaurantMenu/></button>
                             </div>
                             <ul className={`absolute ${isOpen ? '' : 'hidden'} text-white p-3 gap-2 flex flex-col left-[-20px] lg:hidden items-start w-36 bg-black z-20`}>
-                                <li><Link to='/'>Home</Link></li>
-                                <li><Link to='/Menu'>Menu</Link></li>
-                                <li><Link to='/Events'>Events</Link></li>
-                                <li><Link to='/Blog'>Blog</Link></li>
-                                <li><Link to='/Contact_Us'>Contact Us</Link></li>
-                                <li><Link to='/Gift_Cards'>Gift Cards</Link></li>
+                                <li className='link'><Link to='/'>Home</Link></li>
+                                <li className='link'><Link to='/Menu'>Menu</Link></li>
+                                <li className='link'><Link to='/Events'>Events</Link></li>
+                                <li className='link'><Link to='/Blog'>Blog</Link></li>
+                                <li className='link'><Link to='/Contact_Us'>Contact Us</Link></li>
+                                <li className='link'><Link to='/Gift_Cards'>Gift Cards</Link></li>
                             </ul>
                         </div>
                     </div>
